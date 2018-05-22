4563a26cf1884640ccb895feacaca2e14d8747a7
Stephen Elin <39444491+conceptform@users.noreply.github.com>
2018-05-22 17:10:32 -0400

    Add files via upload

---
+++ CNN_Rembrandt.py
@@ -0,0 +1,236 @@
+
+# coding: utf-8
+
+# <b>Algorithmic Phainomena: Deepdream CNN example with Rembrandt's Aristotle</b>
+# 
+# "We must, in the next place, investigate the subject of the dream, and first inquire to which of the faculties of the soul it presents itself, i.e. whether the affection is one which pertains to the faculty of intelligence or to that of sense-perception; for these are the only faculties within us by which we acquire knowledge." 
+# Aristotle, On Dreams
+# 
+# 
+
+# In[1]:
+
+
+from __future__ import print_function
+import os
+from io import BytesIO
+import numpy as np
+from functools import partial
+import PIL.Image
+from IPython.display import clear_output, Image, display, HTML
+import tensorflow as tf
+
+
+# In[2]:
+
+
+model_fn = 'tensorflow_inception_graph.pb'
+graph = tf.Graph()
+sess = tf.InteractiveSession(graph=graph)
+with tf.gfile.FastGFile(model_fn, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+t_input = tf.placeholder(np.float32, name='input')
+imagenet_mean = 117.0
+t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
+tf.import_graph_def(graph_def, {'input':t_preprocessed})
+
+
+# In[3]:
+
+
+layers = [op.name for op in graph.get_operations() if op.type=='Conv2D' and 'import/' in op.name]
+feature_nums = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
+
+print('Number of layers', len(layers))
+print('Total number of feature channels:', sum(feature_nums))
+
+def strip_consts(graph_def, max_const_size=32):
+    strip_def = tf.GraphDef()
+    for n0 in graph_def.node:
+        n = strip_def.node.add() 
+        n.MergeFrom(n0)
+        if n.op == 'Const':
+            tensor = n.attr['value'].tensor
+            size = len(tensor.tensor_content)
+            if size > max_const_size:
+                tensor.tensor_content = tf.compat.as_bytes("<stripped %d bytes>"%size)
+    return strip_def
+  
+def rename_nodes(graph_def, rename_func):
+    res_def = tf.GraphDef()
+    for n0 in graph_def.node:
+        n = res_def.node.add() 
+        n.MergeFrom(n0)
+        n.name = rename_func(n.name)
+        for i, s in enumerate(n.input):
+            n.input[i] = rename_func(s) if s[0]!='^' else '^'+rename_func(s[1:])
+    return res_def
+  
+def show_graph(graph_def, max_const_size=32):
+    if hasattr(graph_def, 'as_graph_def'):
+        graph_def = graph_def.as_graph_def()
+    strip_def = strip_consts(graph_def, max_const_size=max_const_size)
+    code = """
+        <script>
+          function load() {{
+            document.getElementById("{id}").pbtxt = {data};
+          }}
+        </script>
+        <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+        <div style="height:600px">
+          <tf-graph-basic id="{id}"></tf-graph-basic>
+        </div>
+    """.format(data=repr(str(strip_def)), id='graph'+str(np.random.rand()))
+  
+    iframe = """
+        <iframe seamless style="width:800px;height:620px;border:0" srcdoc="{}"></iframe>
+    """.format(code.replace('"', '&quot;'))
+    display(HTML(iframe))
+
+tmp_def = rename_nodes(graph_def, lambda s:"/".join(s.split('_',1)))
+show_graph(tmp_def)
+
+
+# In[4]:
+
+
+layer = 'mixed4d_3x3_bottleneck_pre_relu'
+channel = 135
+
+img_noise = np.random.uniform(size=(224,224,3)) + 100.0
+
+def showarray(a, fmt='jpeg'):
+    a = np.uint8(np.clip(a, 0, 1)*255)
+    f = BytesIO()
+    PIL.Image.fromarray(a).save(f, fmt)
+    display(Image(data=f.getvalue()))
+    
+def visstd(a, s=0.1):
+    return (a-a.mean())/max(a.std(), 1e-4)*s + 0.5
+
+def T(layer):
+    return graph.get_tensor_by_name("import/%s:0"%layer)
+
+def render_naive(t_obj, img0=img_noise, iter_n=20, step=1.0):
+    t_score = tf.reduce_mean(t_obj) 
+    t_grad = tf.gradients(t_score, t_input)[0] 
+    
+    img = img0.copy()
+    for i in range(iter_n):
+        g, score = sess.run([t_grad, t_score], {t_input:img})
+        g /= g.std()+1e-8         
+        img += g*step
+        print(score, end = ' ')
+    clear_output()
+    showarray(visstd(img))
+
+render_naive(T(layer)[:,:,:,channel])
+
+
+# In[5]:
+
+
+img_noise = np.random.uniform(size=(224,224,3)) + 100.0
+
+def render_deepdream(t_obj, img0=img_noise,
+                     iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
+    t_score = tf.reduce_mean(t_obj) 
+    t_grad = tf.gradients(t_score, t_input)[0]
+
+    img = img0
+    octaves = []
+    for i in range(octave_n-1):
+        hw = img.shape[:2]
+        lo = resize(img, np.int32(np.float32(hw)/octave_scale))
+        hi = img-resize(lo, hw)
+        img = lo
+        octaves.append(hi)
+    
+    for octave in range(octave_n):
+        if octave>0:
+            hi = octaves[-octave]
+            img = resize(img, hi.shape[:2])+hi
+        for i in range(iter_n):
+            g = calc_grad_tiled(img, t_grad)
+            img += g*(step / (np.abs(g).mean()+1e-7))
+            print('.',end = ' ')
+        clear_output()
+        showarray(img/255.0)
+
+
+# In[6]:
+
+
+def tffunc(*argtypes):
+    placeholders = list(map(tf.placeholder, argtypes))
+    def wrap(f):
+        out = f(*placeholders)
+        def wrapper(*args, **kw):
+            return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
+        return wrapper
+    return wrap
+
+def resize(img, size):
+    img = tf.expand_dims(img, 0)
+    return tf.image.resize_bilinear(img, size)[0,:,:,:]
+resize = tffunc(np.float32, np.int32)(resize)
+
+
+def calc_grad_tiled(img, t_grad, tile_size=512):
+    sz = tile_size
+    h, w = img.shape[:2]
+    sx, sy = np.random.randint(sz, size=2)
+    img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
+    grad = np.zeros_like(img)
+    for y in range(0, max(h-sz//2, sz),sz):
+        for x in range(0, max(w-sz//2, sz),sz):
+            sub = img_shift[y:y+sz,x:x+sz]
+            g = sess.run(t_grad, {t_input:sub})
+            grad[y:y+sz,x:x+sz] = g
+    return np.roll(np.roll(grad, -sx, 1), -sy, 0)
+
+
+# In[7]:
+
+
+def render_deepdream(t_obj, img0=img_noise,
+                     iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
+    t_score = tf.reduce_mean(t_obj) 
+    t_grad = tf.gradients(t_score, t_input)[0] 
+    img = img0
+    octaves = []
+    for i in range(octave_n-1):
+        hw = img.shape[:2]
+        lo = resize(img, np.int32(np.float32(hw)/octave_scale))
+        hi = img-resize(lo, hw)
+        img = lo
+        octaves.append(hi)
+    
+    for octave in range(octave_n):
+        if octave>0:
+            hi = octaves[-octave]
+            img = resize(img, hi.shape[:2])+hi
+        for i in range(iter_n):
+            g = calc_grad_tiled(img, t_grad)
+            img += g*(step / (np.abs(g).mean()+1e-7))
+            print('.',end = ' ')
+        clear_output()
+        showarray(img/255.0)
+
+
+# In[8]:
+
+
+img0 = PIL.Image.open('Rembrandt_Aristotle.jpg')
+img0 = np.float32(img0)
+showarray(img0/255.0)
+
+
+# In[9]:
+
+
+render_deepdream(tf.square(T('mixed4b')), img0)
+
+
+# "The wolf is at war with the ass, the bull, and the fox, for as being a carnivore, he attacks these other animals.... In regard to wild creatures, some sets are at enmity with other sets at all times and under all circumstances; others, as in the case of man and man, at special times and under incidental circumstances." Aristotle, History of Animals
